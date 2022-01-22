e89288b3d912e813613fd6e0c15899bd77eccf0f
kenshin-shibata <ps0139hk@gmail.com>
2022-01-22 21:03:02 +0900

    add : #1 create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Header from './components/Header'
 
 function App() {
   const name = 'Brand'
   return (
     <div className="App">
       <h1>Hello From React </h1>
+        <Header />
       <h2>Hello {name}</h2>
     </div>
   );
