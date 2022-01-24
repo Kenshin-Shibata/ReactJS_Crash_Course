840c87f7fd85bb2c0eb94c80f36b5caa9b5dad77
kenshin-shibata <ps0139hk@gmail.com>
2022-01-24 21:36:04 +0900

    #30 Add Task Submit

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
         },
     ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+    
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -44,7 +51,7 @@ function App() {
   return (
     <div className="App">
         <Header />
-        <AddTask />
+        <AddTask onAdd={addTask} />
         {tasks.length > 0 ? (<Tasks tasks={tasks}
         onDelete={deleteTask} onToggle={toggleReminder} />
         ) : (
