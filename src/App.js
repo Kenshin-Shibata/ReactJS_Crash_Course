1eb4393c8354a1a40420bdb27c5b7b162855a83f
kenshin-shibata <ps0139hk@gmail.com>
2022-01-25 20:17:08 +0900

    #34 Button Toggle

--- src/App.js
+++ src/App.js
@@ -51,7 +51,10 @@ function App() {
 
   return (
     <div className="App">
-        <Header />
+        <Header onAdd={() => setShowAddTask
+        (!showAddTask)} 
+        showAdd={showAddTask}
+        />
         {showAddTask && <AddTask onAdd={addTask} />}
         {tasks.length > 0 ? (<Tasks tasks={tasks}
         onDelete={deleteTask} onToggle={toggleReminder} />
